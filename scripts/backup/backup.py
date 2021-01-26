c89ac64ca816aec5a68696aea1595e7b5b4f5773
Alexandre Figura <arugifa@users.noreply.github.com>
2021-01-26 10:31:21 +0100

    Fix linters

--- scripts/backup/backup.py
+++ scripts/backup/backup.py
@@ -6,6 +6,7 @@ gpg --gen-key --default-new-key-algo "ed25519/cert,sign+cv25519/encr"
 
 import hashlib
 import logging
+import os
 import sys
 from contextlib import contextmanager
 from datetime import datetime, timezone
@@ -13,7 +14,7 @@ from functools import partial
 from pathlib import Path
 from subprocess import run
 from tempfile import NamedTemporaryFile
-from typing import Callable, List, Optional, Iterator
+from typing import Callable, List, Optional, Iterator, Union, cast
 
 import openstack
 import yaml
@@ -21,7 +22,6 @@ from cached_property import cached_property
 from openstack.exceptions import ResourceNotFound
 from openstack.object_store.v1._proxy import Proxy as ObjectStore
 from openstack.object_store.v1.obj import Object
-from yaml import YAMLError
 
 log = logging.getLogger(__name__)
 
@@ -44,7 +44,7 @@ def retrieve_backup(data_container: str, metadata_container: str) -> 'Backup':
     )
 
 
-def synchronize(directory: Path, backup: 'Backup'):
+def synchronize(directory: str, backup: 'Backup'):
     """Update an online backup to reflect local state.
 
     Files are first compared by "Last Modification Time" to detect new changes.
@@ -54,11 +54,19 @@ def synchronize(directory: Path, backup: 'Backup'):
     Checksums are only computed for files which are not yet detected to be
     in the backup, and after every upload (new files, changes, renaming).
     """
-    # Scan directory to backup.
-    for path in directory.rglob("*"):  # TODO: Do multiprocessing
-        if path.is_file():
-            media = LocalFile(directory, path)
+    cwd = os.getcwd()
+    os.chdir(directory)
 
+    try:
+        # Scan directory to backup.
+        medias = (
+            LocalFile(path.relative_to(directory))
+            for path in Path(directory).rglob('*')
+            if path.is_file()
+        )
+
+        # TODO: Do multiprocessing (01/2021)
+        for media in medias:
             if copy := backup.find_by_path(media):
                 # File already exists in backup.
 
@@ -73,7 +81,7 @@ def synchronize(directory: Path, backup: 'Backup'):
                     if copy.last_modification == media.last_modification:
                         # A copy has been made, or the file has been renamed.
 
-                        if (directory / copy.original_file).exists():
+                        if Path(directory, copy.original_file).exists():
                             # It is a brand new copy.
                             backup.upload(media)
                         else:
@@ -84,8 +92,11 @@ def synchronize(directory: Path, backup: 'Backup'):
                 # File is not in backup yet.
                 backup.upload(media)
 
-    # Archive files not on disk anymore.
-    backup.clean()
+        # Archive files not on disk anymore.
+        backup.clean()
+
+    finally:
+        os.chdir(cwd)
 
 
 # Custom Objects
@@ -117,13 +128,13 @@ class Backup:
         self.object_store = object_store
         self.data_container = data_container
         self.metadata_container = metadata_container
-        self.encryptor = encryptor or Backup.encryptor
-        self.obfuscator = obfuscator or Backup.obfuscator
+        self.encryptor = encryptor or Backup.encryptor  # type: ignore[assignment]
+        self.obfuscator = obfuscator or Backup.obfuscator  # type: ignore[assignment]
 
     # Managers
 
     @contextmanager
-    def open(self, **kwargs) -> None:
+    def open(self, **kwargs) -> Iterator[None]:
         """Open backup for further processing.
 
         All keyword arguments are forwarded to :attr:`.encryptor`.
@@ -136,7 +147,7 @@ class Backup:
         self._checksums = self.load_checksums()  # Can raise YAMLError
 
         _encryptor = self.encryptor
-        self.encryptor = partial(self.encryptor, **kwargs)
+        self.encryptor = partial(self.encryptor, **kwargs)  # type: ignore[assignment]
 
         try:
             yield
@@ -148,14 +159,14 @@ class Backup:
             try:
                 with LocalFile('/tmp', self.MAPPING_FILENAME).open('w') as f:
                     yaml.safe_dump(self._files, f)
-                    self._upload(f, mapping_file, self.metadata_container)
+                    self._upload(f.name, mapping_file, self.metadata_container)
 
                 with LocalFile('/tmp', self.CHECKSUMS_FILENAME).open('w') as f:
                     yaml.safe_dump(self._checksums, f)
-                    self._upload(f, checksums_file, self.metadata_container)
+                    self._upload(f.name, checksums_file, self.metadata_container)
 
             except Exception as exc:
-                log.critical(f"Could not synchronize metadata in backup: {exc}")  # noqa: E501
+                log.critical(f"Could not synchronize metadata in backup: {exc}")
 
                 # TODO: Handle errors (01/2021)
                 # If a backup is run after having been left
@@ -174,57 +185,66 @@ class Backup:
                 LocalFile('/tmp', self.CHECKSUMS_FILENAME).unlink()
 
             finally:
-                self.encryptor = _encryptor
+                self.encryptor = _encryptor  # type: ignore[assignment]
                 del self._files
                 del self._checksums
 
     def clean(self) -> None:
         """Archive in backup files deleted from :attr:`.directory`."""
         # TODO: Don't analyze files already found previously (01/2021)
-        for name in self.files:
-            item = BackupItem(name, backup=self)
 
-            if not (self.directory / item.original_file).exists():
-                self.delete(item)
+        # FIXME: How to clean files stored on several external hard drives? (01/2021)
+        #
+        # When doing a single backup for several hard drives, we want files
+        # to have a relative path in the backup. But when backuping one drive
+        # at a time, it is impossible to detect files which have been deleted.
+        # Thus, we deactivate this function for now, as it only works for backups
+        # with absolute paths.
+        #
+        # for name in self._files:
+        #     item = BackupItem(name, backup=self)
+
+        #     if not (self.directory / item.original_file).exists():
+        #         self.delete(item)
 
     # Finders
 
-     find_by_checksum(self, media: 'Media') -> List['BackupItem']:
-        """Look for files in backup with a similar checksum than `media`."""
+    def find_by_checksum(self, the_file: 'LocalFile') -> List['BackupItem']:
+        """Look for files in backup with a similar checksum than `the_file`."""
         items = []
 
-        for obfuscated_name in self.checksums.get(media.checksum, []):
+        for obfuscated_name in self._checksums.get(the_file.checksum, []):
             item = BackupItem(obfuscated_name, backup=self)
             items.append(item)
 
         return items
 
-    def find_by_path(self, media: 'Media') -> Optional['BackupItem']:
-        """Look for a file in backup with a similar path than `media`."""
-        obfuscated_name = self.obfuscate_filepath(str(media))
+    def find_by_path(self, the_file: 'LocalFile') -> Optional['BackupItem']:
+        """Look for a file in backup with a similar path than `the_file`."""
+        obfuscated_name = self.obfuscate_filepath(str(the_file))
 
         try:
-            assert media.remote_name in self.mapping
+            assert obfuscated_name in self._files
         except AssertionError:
-            return
+            return None
 
         return BackupItem(obfuscated_name, backup=self)
 
     # Actions
 
-    def rename(self, item: 'BackupItem', media: 'Media') -> 'BackupItem':
+    def rename(self, item: 'BackupItem', the_file: 'LocalFile') -> 'BackupItem':
         """Rename file in the backup."""
         assert item.exists(), f"File not in backup: {item.name}"
 
-        obfuscated_name = self.obfuscate_filepath(str(media))
+        obfuscated_name = self.obfuscate_filepath(str(the_file))
         new = self.copy(item, obfuscated_name)
         self.delete(item)
 
-        log.info("Renamed in backup: %s", media)
+        log.info("Renamed in backup: %s", the_file)
 
         return new
 
-    def upload(self, media: 'Media', replace: bool = False) -> 'BackupItem':
+    def upload(self, the_file: 'LocalFile', replace: bool = False) -> 'BackupItem':
         """Upload new file to the backup.
 
         :param replace:
@@ -234,9 +254,9 @@ class Backup:
         """
         # TODO: Properly compute checksum of files > 5GB.
         error = "Cannot backup files bigger than 5gb for now"
-        assert media.stat().st_size < 5368709120, error
+        assert the_file.stat().st_size < 5368709120, error
 
-        obfuscated_name = self.obfuscated_name(media)
+        obfuscated_name = self.obfuscate_filepath(the_file)
         item = BackupItem(obfuscated_name, backup=self)
 
         if replace:
@@ -244,41 +264,41 @@ class Backup:
             assert item.exists(), f"File not in backup: {item.name}"
             self.delete(item)
 
-        self._upload(media, obfuscated_name)
+        self._upload(the_file, obfuscated_name)
 
         # Update metadata.
-        if media.checksum in self.checksums:
-            self.checksums[media.checksum].append(obfuscated_name)
+        if the_file.checksum in self._checksums:
+            self._checksums[the_file.checksum].append(obfuscated_name)
         else:
-            self.checksums[media.checksum] = [obfuscated_name]
+            self._checksums[the_file.checksum] = [obfuscated_name]
 
-        self.files[obfuscated_name] = {
-            'checksum': media.checksum,
-            'file': media.relative_path,
-            'mtime': media.last_modification,
+        self._files[obfuscated_name] = {
+            'checksum': the_file.checksum,
+            'file': str(the_file),
+            'mtime': the_file.last_modification.timestamp(),
         }
 
         if replace:
-            log.info("Uploaded new version for: %s", media)
+            log.info("Uploaded new version for: %s", the_file)
         else:
-            log.info("Uploaded to backup: %s", media)
+            log.info("Uploaded to backup: %s", the_file)
 
         return item
 
-    def upload_new_version(self, media: 'Media') -> 'BackupItem':
+    def upload_new_version(self, the_file: 'LocalFile') -> 'BackupItem':
         """Update a file in the backup.
 
         A copy of the previous version is kept for 6 months.
         """
-        obfuscated_name = self.obfuscated_name(media)
+        obfuscated_name = self.obfuscate_filepath(the_file)
         item = BackupItem(obfuscated_name, backup=self)
         assert item.exists(), f"File not in backup: {item.name}"
 
         # TODO: Properly compute checksum of files > 5GB (01/2021)
-        error = "Cannot backup files bigger than 5gb for now: {media}"
-        assert media.stat().st_size < 5368709120, error
+        error = "Cannot backup files bigger than 5gb for now: {the_file}"
+        assert the_file.stat().st_size < 5368709120, error
 
-        return self.upload(media, replace=True)
+        return self.upload(the_file, replace=True)
 
     # Helpers
 
@@ -291,8 +311,8 @@ class Backup:
             f'{self.data_container} {item.name}',
             check=True,  # TODO: Handle errors and check ETAG (01/2021)
         )
-        self.files[target] = self.files[item.name]
-        self.checksums[item.checksum].append(target)
+        self._files[target] = self._files[item.name]
+        self._checksums[item.checksum].append(target)
 
         return BackupItem(target, backup=self)
 
@@ -317,12 +337,12 @@ class Backup:
         )
 
         # Update metadata.
-        self.files.pop(item.name)
+        self._files.pop(item.name)
 
-        if len(self.checksums[item.checksum]) == 1:
-            self.checksums.pop(item.checksum)
+        if len(self._checksums[item.checksum]) == 1:
+            self._checksums.pop(item.checksum)
         else:
-            self.checksums[item.checksum].remove(item.name)
+            self._checksums[item.checksum].remove(item.name)
 
     def load_checksums(self):
         checksums_file = self.obfuscate_filepath(self.CHECKSUMS_FILENAME)
@@ -351,7 +371,7 @@ class Backup:
     # Petits Cachotiers
 
     @contextmanager
-    def encrypt_file(self, path: 'LocalFile') -> Iterator[str]:
+    def encrypt_file(self, path: 'LocalFile') -> Iterator['LocalFile']:
         encrypted = NamedTemporaryFile()
         self.encryptor(str(path), encrypted.name)
 
@@ -360,8 +380,8 @@ class Backup:
         finally:
             encrypted.close()
 
-    def obfuscate_filepath(self, path: str) -> str:
-        return self.obfuscator(path)
+    def obfuscate_filepath(self, path: Union[str, Path]) -> str:
+        return self.obfuscator(str(path))
 
     # Private Thingies
 
@@ -369,23 +389,23 @@ class Backup:
         container = container or self.data_container
         return self.object_store.get_object(name, container=container)
 
-    def _get(self, name: str, container: str = None) -> [Object]:
+    def _get(self, name: str, container: str = None) -> Object:
         container = container or self.data_container
         return self.object_store.get_object_metadata(name, container)
 
     def _upload(
             self,
-            something: Union[bytes, 'LocalFile'],
+            something: Union[bytes, str, 'LocalFile'],
             target: str,
             container: str = None) -> Object:
         container = container or self.data_container
 
-        if isinstance(something, LocalFile):
-            path = something
-        else:
+        if isinstance(something, bytes):
             tmp = NamedTemporaryFile()
             tmp.write(something)
             path = LocalFile('/tmp', tmp.name)
+        else:
+            path = cast(LocalFile, something)
 
         with self.encrypt_file(path) as encrypted:
             while self.object_store.upload_object(
@@ -399,7 +419,7 @@ class Backup:
                     log.warning(
                         f"Upload not completed successfully. "
                         f"Remote copy appears to be corrupted. "
-                        f"Trying again: {something}"
+                        f"Trying again: {path}"
                     )
 
         return upload
@@ -413,28 +433,25 @@ class BackupItem:
     # Properties
 
     @property
-    def checksum(self):
-        return self.backup.files[self.name]['checksum']
+    def checksum(self) -> str:
+        return self.backup._files[self.name]['checksum']
 
     @property
-    def last_modification(self):
-        return self.backup.files[self.name]['mtime']
+    def last_modification(self) -> datetime:
+        mtime = self.backup._files[self.name]['mtime']
+        return datetime.fromtimestamp(mtime, tz=timezone.utc)
 
     @property
-    def original_file(self):
-        return self.backup.files[self.name]['file'],
+    def original_file(self) -> str:
+        return self.backup._files[self.name]['file']
 
     # Methods
 
-    def exists(self):
-        return self.name in self.backup.files
+    def exists(self) -> bool:
+        return self.name in self.backup._files
 
 
 class LocalFile(Path):
-    def __init__(self, directory: Path, path: Union[Path, str]):
-        Path.__init__(self, directory, path)
-        self.directory = directory
-
     @cached_property
     def checksum(self) -> str:
         # Thanks to https://stackoverflow.com/a/3431838/2987526
@@ -447,18 +464,14 @@ class LocalFile(Path):
         return md5sum.hexdigest()
 
     @property
-    def last_modification(self) -> float:
-        return datetime.fromtimetamp(self.stat().st_mtime, tz=timezone.utc)
-
-    @property
-    def relative_path(self) -> Path:
-        return self.relative_to(self.directory)
+    def last_modification(self) -> datetime:
+        return datetime.fromtimestamp(self.stat().st_mtime, tz=timezone.utc)
 
 
 # Helper Functions
 
-def encrypt_with_gpg(src: str, dst: str, *, gpg_key: str) -> None:
-    cli = f'gpg -e -r {gpg_key} -o {src} {dst}'
+def encrypt_with_gpg(src: str, dst: str, *, recipient: str) -> None:
+    cli = f'gpg -e -r {recipient} -o {src} {dst}'
     run(cli, check=True)
 
 
@@ -480,8 +493,12 @@ if __name__ == '__main__':
     console.setFormatter(log_format)
     log.addHandler(console)
 
+    print("Starting backup...")
+
     # Run backup.
     backup = retrieve_backup(data_container, metadata_container)
 
-    with backup.open(gpg_key):
+    with backup.open(recipient=gpg_key):
         synchronize(directory, backup)
+
+    print("Backup done. See you next time!")
